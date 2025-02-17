beea9b6c6ba7ef2c92868734db273abe03422cb0
Igor Davydenkov <igor.davydenkov@gmail.com>
2025-02-17 21:46:24 +0000

    add env config

--- bot.py
+++ bot.py
@@ -2,11 +2,12 @@ import asyncio
 import logging
 from aiogram import Bot, Dispatcher, types
 from aiogram.filters.command import Command
+from config_reader import config
 
 # Включаем логирование, чтобы не пропустить важные сообщения
 logging.basicConfig(level=logging.INFO)
 # Объект бота
-bot = Bot(token="7530890044:AAEnQ1HDt7_Z4UbnFHVnYdsFP9SGniyRsJE")
+bot = Bot(token=config.bot_token.get_secret_value())
 # Диспетчер
 dp = Dispatcher()
 
